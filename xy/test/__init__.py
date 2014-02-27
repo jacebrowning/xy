d78410a003889d3d9f117f14afcb4a72f566df39
Josh Friend <josh@fueledbycaffeine.com>
2014-02-26 22:08:57 -0500

    Reformatted docstrings to pass pep257

--- xy/test/__init__.py
+++ xy/test/__init__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-"""
-Tests for the xy package.
-"""
+"""Tests for the xy package."""
 
 ENV = 'TEST_INTEGRATION'  # environment variable to enable integration tests
 REASON = "'{0}' variable not set".format(ENV)
